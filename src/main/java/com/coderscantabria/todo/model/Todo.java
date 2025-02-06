64482d7830a7f7d090e512e47f0a368ec837b3e8
Juan José González Abril <juanjo@gonzabril.es>
2025-02-06 16:04:52 +0100

    Configure Postgres database

--- src/main/java/com/coderscantabria/todo/model/Todo.java
+++ src/main/java/com/coderscantabria/todo/model/Todo.java
@@ -10,11 +10,15 @@ public class Todo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "title", nullable = false, length = 50)
     private String title;
 
+    @Column(name = "description", length = 500)
     private String description;
 
+    @Column(name = "completed", nullable = false)
     private boolean completed;
 }
